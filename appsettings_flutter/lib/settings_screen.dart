6562ecb03f749485319662ca7c7b6bf8741efaaa
kba1986cr <haroldsa1986@hotmail.com>
2025-06-06 22:05:33 -0600

    Ajustes drkmode, languge y slice

--- appsettings_flutter/lib/settings_screen.dart
+++ appsettings_flutter/lib/settings_screen.dart
@@ -35,12 +35,82 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 color: Colors.lightGreen,
               ),
               child: SwitchListTile(
-                title: const Text('Modo oscuro'),
+                title: Text('Modo oscuro'),
                 value: _darkMode,
                 onChanged: (darkmode) {
                   setState(() => _darkMode = darkmode);
                 },
+                secondary: const Icon(Icons.dark_mode, color: Colors.blueGrey),
               ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 2),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.lightGreen,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField(
+                  value: _language,
+                  items: [
+                    DropdownMenuItem(value: 'es', child: Text('Español')),
+                    DropdownMenuItem(value: 'en', child: Text('Inglés')),
+                    DropdownMenuItem(value: 'ch', child: Text('Chino')),
+                  ],
+                  onChanged: (language) {
+                    // if (language != null) {
+                    //   setState(() => _language = language);
+                    // }
+                    setState(() => _language = language!);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Idioma',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 2),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.lightGreen,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Tamaño de la fuente ${_fontSize.toStringAsFixed(0)}",
+                  style: TextStyle(fontSize: _fontSize),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 2),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.lightGreen,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Slider(
+                  min: 14,
+                  max: 30,
+                  value: _fontSize,
+                  onChanged: (fontSize) {
+                    setState(() => _fontSize = fontSize);
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Aquí puedes agregar la lógica para guardar los ajustes
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('Ajustes Guardados')));
+              },
+              child: Text('Guardar ajustes'),
             ),
           ],
         ),
